5923cefd97829c909aa56975b6fcf1263edff857
ttts1263 <ttts1263@gmail.com>
2023-09-20 20:31:14 +0900

    feat: add comment name, style comment

--- index.js
+++ index.js
@@ -3,16 +3,31 @@
 // document.querySelector("#commentButton")  // tag
 // document.querySelector("[name='#commentButton']") // name
 
-var commentButton = document.querySelector("#commentButton");
-var commentInput = document.querySelector("#commentInput");
+//댓글 버튼, 댓글 인풋 불러오기
+var commentButton = document.querySelector(".commentButton");
+var commentName = document.querySelector(".commentName");
+var commentText = document.querySelector(".commentText");
 console.log(commentButton);
-console.log(commentInput);
+console.log(commentName);
+console.log(commentText);
 
+//댓글 버튼에 click 이벤트 추가하기
 commentButton.onclick = function () {
-  console.log(commentInput.value);
-  let div = document.createElement("div");
-  div.innerHTML = commentInput.value;
+  let commentDiv = document.createElement("commentDiv");
+  commentDiv.className = "commentDiv";
+  let nameSpan = document.createElement("span");
+  nameSpan.className = "nameSpan";
+  let textSpan = document.createElement("span");
+  textSpan.className = "textSpan";
+  console.log(commentDiv);
+  console.log(nameSpan);
+  console.log(textSpan);
 
-  let main = document.querySelector("main");
-  main.appendChild(div);
+  commentDiv.appendChild(nameSpan);
+  commentDiv.appendChild(textSpan);
+  nameSpan.innerHTML = commentName.value;
+  textSpan.innerHTML = commentText.value;
+
+  let commentList = document.querySelector(".commentList");
+  commentList.appendChild(commentDiv);
 };
