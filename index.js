4153a56d386d8d083a0dcf8a4a5a93c17cbc8033
hoodie <popping2606@gmail.com>
2023-09-29 19:15:03 +0900

    refactor: variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // document.querySelector('#commentButton') // id
-// document.querySelector('.#commentButton')  // class
-// document.querySelector('#commentButton')  // tag
-// document.querySelector('[name='#commentButton']') // name
+// document.querySelector('.commentButton')  // class
+// document.querySelector('commentButton')  // tag
+// document.querySelector('[name='commentButton']') // name
 
 // 배열
 // const array = [1, 2, 3, 4, 5];
@@ -17,77 +17,63 @@
 
 // 로컬스토리지에서 댓글 불러오기
 // localStorage에서 가져와서 localstring으로 변환
-const savedData = localStorage.getItem("my-page-comment")
-const parsedData = JSON.parse(savedData)
+const localComments = localStorage.getItem('my-page-comments')
+const parsedLocalComments = JSON.parse(localComments)
 
-console.log(parsedData)
 // object array 형태로 변수에 들어있음
 // 불러온 데이터를 화면에 그려주기
-const commentDiv = document.createElement("commentDiv")
-commentDiv.className = "commentDiv"
-const nameSpan = document.createElement("span")
-nameSpan.className = "nameSpan"
-const textSpan = document.createElement("span")
-textSpan.className = "textSpan"
-console.log(commentDiv)
-console.log(nameSpan)
-console.log(textSpan)
+const commentDiv = document.createElement('commentDiv')
+commentDiv.className = 'commentDiv'
+const nameSpan = document.createElement('span')
+nameSpan.className = 'nameSpan'
+const textSpan = document.createElement('span')
+textSpan.className = 'textSpan'
 
 commentDiv.appendChild(nameSpan)
 commentDiv.appendChild(textSpan)
-nameSpan.innerHTML = parsedData[0].name
-textSpan.innerHTML = parsedData[0].text
+if (parsedLocalComments?.[0] !== undefined) {
+  nameSpan.innerHTML = parsedLocalComments?.[0].name
+  textSpan.innerHTML = parsedLocalComments?.[0].text
+}
 
-const commentList = document.querySelector(".commentList")
-commentList.appendChild(commentDiv)
+const commentListDiv = document.querySelector('.commentList')
+commentListDiv.appendChild(commentDiv)
 // 댓글 불러오기 완료 //
 
 // 댓글 버튼, 댓글 인풋 불러오기
-const commentButton = document.querySelector(".commentButton")
-const commentName = document.querySelector(".commentName")
-const commentText = document.querySelector(".commentText")
-console.log(commentButton)
-console.log(commentName)
-console.log(commentText)
+const commentButton = document.querySelector('.commentButton')
+const commentNameInput = document.querySelector('.commentName')
+const commentTextInput = document.querySelector('.commentText')
 
 // 댓글을 저장할 겍체 배열
-const data = []
+const comments = []
 
 // 댓글 버튼에 click 이벤트 추가하기
 commentButton.onclick = function () {
-  const commentDiv = document.createElement("commentDiv")
-  commentDiv.className = "commentDiv"
-  const nameSpan = document.createElement("span")
-  nameSpan.className = "nameSpan"
-  const textSpan = document.createElement("span")
-  textSpan.className = "textSpan"
-  console.log(commentDiv)
-  console.log(nameSpan)
-  console.log(textSpan)
+  const commentDiv = document.createElement('commentDiv')
+  commentDiv.className = 'commentDiv'
+  const nameSpan = document.createElement('span')
+  nameSpan.className = 'nameSpan'
+  const textSpan = document.createElement('span')
+  textSpan.className = 'textSpan'
 
   commentDiv.appendChild(nameSpan)
   commentDiv.appendChild(textSpan)
-  nameSpan.innerHTML = commentName.value
-  textSpan.innerHTML = commentText.value
+  nameSpan.innerHTML = commentNameInput.value
+  textSpan.innerHTML = commentTextInput.value
 
-  const commentList = document.querySelector(".commentList")
-  commentList.appendChild(commentDiv)
+  const commentListDiv = document.querySelector('.commentList')
+  commentListDiv.appendChild(commentDiv)
 
   // 댓글을 item에 담고 , data에 추가하기
 
-  const item = {
-    name: commentName.value,
-    text: commentText.value,
+  const comment = {
+    name: commentNameInput.value,
+    text: commentTextInput.value,
   }
-  data.push(item)
+  comments.push(comment)
 
   // string으로 변환해서 저장
-  const stringData = JSON.stringify(data)
-  localStorage.setItem("my-page-comment", stringData)
+  const stringComments = JSON.stringify(comments)
+  localStorage.setItem('my-page-comments', stringComments)
 }
-
-// 로컬스토리지
-localStorage.setItem("과일", "사과")
-const fruit = localStorage.getItem("과일")
-console.log(fruit)
-localStorage.removeItem("과일")
