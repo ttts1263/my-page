b72472ff7ffd3d0cd9a91555c513965c17233c05
hoodie <popping2606@gmail.com>
2023-09-29 20:03:40 +0900

    feat: study function

--- index.js
+++ index.js
@@ -15,65 +15,66 @@
 //   { name: 3, text: "ccc" },
 // ];
 
-// 로컬스토리지에서 댓글 불러오기
-// localStorage에서 가져와서 localstring으로 변환
-const localComments = localStorage.getItem('my-page-comments')
-const parsedLocalComments = JSON.parse(localComments)
+// 즉시실행함수(IIFE)로 스코프 제한하기
+(function () {
+  // 로컬스토리지에서 댓글 불러오기
+  // localStorage에서 가져와서 localstring으로 변환
+  const localComments = localStorage.getItem('my-page-comments');
+  const parsedLocalComments = JSON.parse(localComments);
 
-// object array 형태로 변수에 들어있음
-// 불러온 데이터를 화면에 그려주기
-const commentDiv = document.createElement('commentDiv')
-commentDiv.className = 'commentDiv'
-const nameSpan = document.createElement('span')
-nameSpan.className = 'nameSpan'
-const textSpan = document.createElement('span')
-textSpan.className = 'textSpan'
+  // object array 형태로 변수에 들어있음
+  // 불러온 데이터를 화면에 그려주기
+  renderComments(parsedLocalComments);
 
-commentDiv.appendChild(nameSpan)
-commentDiv.appendChild(textSpan)
-if (parsedLocalComments?.[0] !== undefined) {
-  nameSpan.innerHTML = parsedLocalComments?.[0].name
-  textSpan.innerHTML = parsedLocalComments?.[0].text
-}
+  // 댓글을 저장할 겍체 배열
+  const comments = [];
 
-const commentListDiv = document.querySelector('.commentList')
-commentListDiv.appendChild(commentDiv)
-// 댓글 불러오기 완료 //
+  // 댓글 버튼, 댓글 인풋 불러오기
+  const commentButton = document.querySelector('.commentButton');
 
-// 댓글 버튼, 댓글 인풋 불러오기
-const commentButton = document.querySelector('.commentButton')
-const commentNameInput = document.querySelector('.commentName')
-const commentTextInput = document.querySelector('.commentText')
+  // 댓글 버튼에 click 이벤트 추가하기
+  commentButton.onclick = function () {
+    const commentNameInput = document.querySelector('.commentName');
+    const commentTextInput = document.querySelector('.commentText');
+    renderComments([
+      {
+        name: commentNameInput.value,
+        text: commentTextInput.value,
+      },
+    ]);
 
-// 댓글을 저장할 겍체 배열
-const comments = []
+    // 댓글을 item에 담고 , data에 추가하기
 
-// 댓글 버튼에 click 이벤트 추가하기
-commentButton.onclick = function () {
-  const commentDiv = document.createElement('commentDiv')
-  commentDiv.className = 'commentDiv'
-  const nameSpan = document.createElement('span')
-  nameSpan.className = 'nameSpan'
-  const textSpan = document.createElement('span')
-  textSpan.className = 'textSpan'
+    const comment = {
+      name: commentNameInput.value,
+      text: commentTextInput.value,
+    };
+    comments.push(comment);
 
-  commentDiv.appendChild(nameSpan)
-  commentDiv.appendChild(textSpan)
-  nameSpan.innerHTML = commentNameInput.value
-  textSpan.innerHTML = commentTextInput.value
+    // string으로 변환해서 저장
+    const stringComments = JSON.stringify(comments);
+    localStorage.setItem('my-page-comments', stringComments);
+  };
+})();
 
-  const commentListDiv = document.querySelector('.commentList')
-  commentListDiv.appendChild(commentDiv)
+// utils 함수들 //
 
-  // 댓글을 item에 담고 , data에 추가하기
+// comments를 입력받아서 화면에 그려주는 함수
+function renderComments(comments) {
+  const commentDiv = document.createElement('commentDiv');
+  commentDiv.className = 'commentDiv';
+  const nameSpan = document.createElement('span');
+  nameSpan.className = 'nameSpan';
+  const textSpan = document.createElement('span');
+  textSpan.className = 'textSpan';
 
-  const comment = {
-    name: commentNameInput.value,
-    text: commentTextInput.value,
+  commentDiv.appendChild(nameSpan);
+  commentDiv.appendChild(textSpan);
+  if (comments?.[0] !== undefined) {
+    nameSpan.innerHTML = comments?.[0].name;
+    textSpan.innerHTML = comments?.[0].text;
   }
-  comments.push(comment)
 
-  // string으로 변환해서 저장
-  const stringComments = JSON.stringify(comments)
-  localStorage.setItem('my-page-comments', stringComments)
+  const commentListDiv = document.querySelector('.commentList');
+  commentListDiv.appendChild(commentDiv);
 }
